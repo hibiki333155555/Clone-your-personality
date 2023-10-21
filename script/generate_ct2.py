0299ed6110c85d70c7e75abf40ffaaa1f2ab9d9f
andy <takuyarossi77@gmail.com>
2023-10-22 01:46:58 +0900

    a

--- script/generate_ct2.py
+++ script/generate_ct2.py
@@ -2,17 +2,43 @@ import json
 from peft import PeftModel
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 import ctranslate2
-import torch
+import boto3
+import uuid
 
 class MyModel:
     def __init__(self, model_name, tokenizer_name, quant_model):
         self.generator = ctranslate2.Generator(quant_model, device="auto")
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, use_fast=False)
+    # データベースに会話を追加
+    def insert_conversation(self, speaker, text):
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('llm_conversations')
         
-    def prompt(instruction):
-        p = [
-            {"speaker": "ユーザー", "text": instruction},
-        ]
+        response = table.put_item(
+            Item={
+                'conversationID': str(uuid.uuid4()),
+                'speaker': speaker,
+                'text': text
+            }
+        )
+        return response
+    # データベースから過去の会話を取得
+    def get_past_conversations(self):
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('llm_conversations')
+        
+        response = table.scan()
+        items = response.get('Items', [])
+                
+        conversations = [{"speaker": item["speaker"], "text": item["text"]} for item in items]
+        
+        print(conversations)
+        return conversations
+
+    def prompt(self, instruction):
+        past_conversations = self.get_past_conversations()
+        p = past_conversations + [{"speaker": "ユーザー", "text": instruction}]
+        
         p = [f"{uttr['speaker']}: {uttr['text']}" for uttr in p]
         p = "<NL>".join(p)
         p = p + "<NL>" + "システム: "
@@ -20,7 +46,7 @@ class MyModel:
     
     def generate(self, instruction, input=None, maxTokens=256):
         # TokenizationとCTRANSLATE2の入力形式への変換
-        tokens = self.tokenizer.convert_ids_to_tokens(self.tokenizer.encode(MyModel.prompt(instruction),add_special_tokens=False,))
+        tokens = self.tokenizer.convert_ids_to_tokens(self.tokenizer.encode(self.prompt(instruction),add_special_tokens=False,))
         results = self.generator.generate_batch(
             [tokens],
             max_length=256,
@@ -37,5 +63,11 @@ quant_model = "../merged-rinnna-ppo-int8_ct2"
 tokenizer_name = model_name
 my_model = MyModel(model_name, tokenizer_name, quant_model)
 
-print(my_model.generate("人生ってなんだと思う？"))
+print(my_model.generate("なんのチーズがすき？"))
 
+"""
+my_model.insert_conversation("ユーザー", "なんのチーズがすき？")
+responce_text = my_model.generate("なんのチーズがすき？")
+print(responce_text)
+my_model.insert_conversation("システム", responce_text)
+"""
